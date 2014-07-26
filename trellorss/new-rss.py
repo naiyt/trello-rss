d458cb447ddd654211297f1bd6fbcd1b693b6bcf
Nate Collings <nate@natecollings.com>
2014-07-26 11:10:42 -0700

    Flow update

---
+++ trellorss/new-rss.py
@@ -0,0 +1,14 @@
+class TrelloRSS:
+    def __init__(self):
+        pass
+
+    def to_rss(self, rss):
+        pass
+
+    def get_rss_info(self):
+        '''Makes an API request'''
+        pass
+
+    def _format_rss(self, rss):
+        '''Formats rss from either a hook or API call'''
+        pass
